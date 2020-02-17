f1396977b71460a5eb1e4b4e7cb5e68c3584cebe
Izaak Baker <izaak_baker@alumni.brown.edu>
2020-02-17 00:32:35 -0500

    better identifiers, include the starting token

--- markov.clj
+++ markov.clj
@@ -10,14 +10,14 @@
         (apply merge-with #(vec (concat %1 %2))
         (map pair-to-map tokens shifted))))
 
-(defn generate [graph element result]
-    (let [chosen (rand-nth (graph element))]
-        (if (nil? chosen)
+(defn generate [graph last-token result]
+    (let [chosen-token (rand-nth (graph last-token))]
+        (if (nil? chosen-token)
             result
-            (recur graph chosen (str result " " chosen)))))
+            (recur graph chosen-token (str result " " chosen-token)))))
 
 (defn -main [thearg] 
     (let [tokens (tokenize thearg) graph (subsequents tokens) starting-point (rand-nth tokens)]
-        (println (generate graph starting-point ""))))
+        (println (generate graph starting-point starting-point))))
 
 (apply -main *command-line-args*)
